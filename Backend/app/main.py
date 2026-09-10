0158f29b1db6818b5fc0854ce2905bf9fce7eb72
sanoj c sam <sanojcsam123@gmail.com>
2026-09-10 20:45:14 +0530

    modified:   Backend/app/main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -19,6 +19,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://mcp-autonomous-ai-data-scientist1.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
